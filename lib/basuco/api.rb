ef49e05278bc84006c03ecd5e60831c8dbf68d66
Caleb Bron <calebbron@gmail.com>
2011-10-09 11:20:58 -0700

    status done

--- lib/basuco/api.rb
+++ lib/basuco/api.rb
@@ -5,18 +5,36 @@ module Basuco
 
   class Api
     include Request
-    public
     def initialize(options = {:host => 'http://www.freebase.com', :username => 'un', :password => 'pw'})
       @host = options[:host]
       @username = options[:username]
       @password = options[:password]
-      Basuco.Api = self
+      Basuco.api = self
     end
 
-    def check_status
-      http_request status_service_url
+    #yes or no
+    def status?
+      response = http_request status_service_url
+      result = JSON.parse response
+      return result["code"] == "/api/status/ok"
     end
 
+    #hash of all statuses
+    def check_statuses
+      response = http_request status_service_url
+      result = JSON.parse response
+      result
+    end
 
+    def api(options = {})
+      options.merge!({:query => query})
+      
+      response = http_request search_service_url, options
+      result = JSON.parse response
+      
+      handle_read_error(result)
+      
+      result['result']
+    end
   end
 end
