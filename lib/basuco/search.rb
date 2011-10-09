73b62793d7c0ebbae74c8cc947c5d8de27167f8f
Caleb Bron <calebbron@gmail.com>
2011-10-09 10:17:55 -0700

    added request

--- lib/basuco/search.rb
+++ lib/basuco/search.rb
@@ -1,4 +1,5 @@
 module Basuco
+  require 'request'
   class << self
     attr_accessor :search
   end
@@ -44,7 +45,7 @@ module Basuco
       :blurb => '/api/trans/blurb/guid/',
       :raw => '/api/trans/raw/guid/',
       :login => '/api/account/login', #not done
-      :logout => '/api/account/logout' #not done
+      :logout => '/api/account/logout', #not done
       :upload => '/api/service/upload',
       :topic => '/experimental/topic',
       :search => '/api/service/search',
@@ -146,35 +147,5 @@ module Basuco
       result['result']
     end
 
-    protected
-    # returns parsed json response from freebase mqlread service
-    def get_query_response(query, cursor=nil)
-      envelope = { :qname => {:query => query, :escape => false }}
-      envelope[:qname][:cursor] = cursor if cursor
-      
-      response = http_request mqlread_service_url, :queries => envelope.to_json
-      result = JSON.parse response
-      inner = result['qname']
-      handle_read_error(inner)
-      inner
-    end
-    
-    # encode parameters
-    def params_to_string(parameters)
-      parameters.keys.map {|k| "#{URI.encode(k.to_s)}=#{URI.encode(parameters[k].to_s)}" }.join('&')
-    end
-    
-    # does the dirty work
-    def http_request(url, parameters = {})
-      params = params_to_string(parameters)
-      url << '?'+params unless params !~ /\S/
-            
-      return Net::HTTP.get_response(::URI.parse(url)).body
-      
-      fname = "#{MD5.md5(params)}.mql"
-      open(fname,"w") do |f|
-        f << response
-      end
-    end
   end # class Search
 end # module Basuco
